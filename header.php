e7722c8b42fb6713c8c38fdddc0f790b3bf4e3e8
nurazhad <azhadnurahmann@outlook.com>
2021-01-28 15:29:40 +0700

    adding file

---
+++ header.php
@@ -0,0 +1,23 @@
+<header class="masthead">
+        <!-- <h3 class="text-muted"></h3> -->
+
+        <nav class="navbar navbar-expand-md navbar-light rounded mb-3" style="background-color: #e9ecef;">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav text-md-center nav-justified w-100">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><b>Home </b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="diagnosa.php"><b>Diagnosa</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="daftar.php"><b>Daftar Penyakit</b></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <!-- <img src="bg.png" width="1110" height="350" style="margin-left: 0px; margin-bottom: 10px;" />  -->
